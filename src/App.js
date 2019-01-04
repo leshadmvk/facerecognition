beb461f740c3a2e7a17d3c27f2a1908ddf9297fc
leshadmvk <leshadmvk@gmail.com>
2019-01-04 22:57:16 +0300

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-import Navigation from './Components/Navigation/Navigation'
-import Logo from './Components/Logo/Logo'
-import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
-import Rank from './Components/Rank/Rank'
+//COMPONENTS
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
+import Rank from './Components/Rank/Rank';
+import SignIn from './Components/SignIn/SignIn';
+import Register from './Components/Register/Register';
+//API's
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+//CSS
 import './App.css';
-import Particles from 'react-particles-js'
+
+const app = new Clarifai.App({
+ apiKey: '3aea2099fdc54eb08639bd02310995b2'
+});
+
 
 const particlesOptions = {
       particles: {
@@ -18,17 +30,80 @@ const particlesOptions = {
     }
   }
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      imageUrl:'',
+      box: {},
+      route: 'signin',
+      isSignedIn: false
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+   const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+   const image = document.getElementById('inputImage');
+   const width = Number(image.width);
+   const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL, 
+      this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))// do something wi  th response
+    .catch(err => console.log(err));
+  }
+
+  onRouteChange = (route) => {
+    if(route === 'signout'){
+      this.setState({isSignedIn:false})
+    }else{
+      this.setState({isSignedIn:true})
+    }
+    this.setState({route: route});
+  }
+
   render() {
+    const {isSignedIn, imageUrl, route, box} = this.state;
     return (
       <div className="App">
         <Particles className='particles'
           params={particlesOptions}
         />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm />
-        {/*  <Facerecognition />*/}
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+      {route === 'home' 
+          ? <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm 
+                onInputChange={this.onInputChange} 
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition box={box} imageUrl={imageUrl}  />
+            </div>
+          : route === 'signin'
+              ? <SignIn onRouteChange={this.onRouteChange}/>
+              : <Register onRouteChange={this.onRouteChange}/>
+          
+          
+      }
       </div>
     );
   }
--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './ImageLinkForm.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
 	return(
 		<div>
 			<p className='f3'>
 				{'This APP will detect faces in your pictures'}</p>
 			<div className='center'>
 				<div className='form center pa4 br3 shadow-3'>
-					<input className='f4 pa2 w-70 center'type='text'/>
-					<button className='w-30 grow f4 link ph3 pv2 dib black bg-light-gray'>Detect</button>
+					<input className='f4 pa2 w-70 center'type='tex' onChange={onInputChange}/>
+					<button
+						className='w-30 grow f4 link ph3 pv2 dib black bg-light-gray'
+						onClick={onButtonSubmit} 
+					>Detect</button>
 				</div>
 			</div>
 		</div>
